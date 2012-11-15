27f63e895ed705d26098b94539a8209782a09e94
Assontia Patient <assontia@gmail.com>
2012-11-15 18:39:58 +0100

    Edit autocar

---
+++ edit-autocar.php
@@ -0,0 +1,236 @@
+<?php 
+$page = 'home';
+$texte_input_recherche = 'Entrez une localit&eacute;';
+include_once "inc/recherche.ann.php";
+include_once "inc/localites.ann.php";
+include_once "inc/resultats.ann.php"; 
+include_once "inc/haut.inc.php";
+?>
+		
+		<div id="main-container" class="clearfix">
+		
+		    <div class="tete">
+			    <h2><span>CONNEX ALLIER</span> - MARSEILLE</h2>
+			</div>
+			<div class="contentTab">
+			    <div class="tab">
+				    <ul class="onglets">    
+						<li class="onglet1 actif">Pr&eacute;sentation</li>
+					    <li class="onglet2">Notre Flotte</li>
+					    <li class="onglet3">Services</li>
+					    <li class="onglet4">Infos & Contacts</li>
+					    <li class="onglet5">Votre profil</li>
+				    </ul>
+					<div class="tabs">
+					    <div class="tab1">
+					        <p>
+						        <img class="imageFlottante" src="img/car2.png" height="210px" alt="autocar"/>
+					        </p>
+						
+					    	<p>
+						        L.C.J - les cars Jouquin  sont implant&eacute;s depuis 80 ans sur la ville
+							    nouvelle de Saint-Quentin-enYvelines, au coeur du Grand Parc de
+							    Versailles &agrave; vingt kilom&egrave;metres de Paris, et exploitent un cenre moderne
+							    d'autocars: <br/>
+							
+							    <br/>Servcices commercial et exploitation, personnel de conduite, atelier,
+							    entretien. <br/>
+                            
+                                <br/>Son expertise et sa rigueur s&eacute;curitaire en mati&egrave;re de multi transports &agrave;
+                                travers l'Europe ont naturellement dessin&eacute; sont titre : Transporteur
+                                Europ&eacute;en.
+                                &agrave; votre service, pour que votre prestation soit unique et son &eacute;x&eacute;cution
+                                r&eacute;gl&eacute;e
+                                &agrave; votre service, pour que votre prestation soit unique et son &eacute;x&eacute;tion
+                                r&eacute;gl&eacute;e dans les moindres d&eacute;tails.<br/>
+                            
+                                <br/><strong>Une &eacute;quipe de professionels du Transport</strong><br/>
+      
+                                <br/>Encad&eacute;e par un Directeur et un Chef d'exploitation d'exp&eacute;rience, notre &eacute;quipe d'Exploitation coordonne 40 chaufeurs de remise
+                                &agrave; vos soins et &agrave; votre service, pour que votre prestation soit unique et son ex&eacute;cution r&eacute;gl&eacute;e dans les moindres d&eacute;tails.	<br/>						
+						
+			    			    <br/><strong>Une large gamme de 50 v&eacute;hicules adapt&eacute;s &agrave; vos besoins de Transport</strong><br/>
+						
+				    		    <br/>L.C.J met &agrave; votre disposition une flotte d'autocars de 8 &agrave; 63 places d'identit&eacute; visuelle uniforme et de moins de 6 ans. Notre
+					    		passion de l'autocar nous a conduit &agrave; orienter notre politique d'entreprise : un parc haut de gamme, r&eacute;cent et soign&eacute;.<br/>
+						
+						        <br/>L'ensemble de nos v&eacute;hicules est liv&eacute; par les deux plus grands constucteurs Evobus (Setra et Mercedes-Benz) et Irisbus
+							    (Renault-Iveco), ce qui permet de b&eacute;n&eacute;ficier des derni&egrave;res avanc&eacute;es technologiques en mati&egrave;re de s&eacute;curit&eacute; et de confort. 
+						    </p>
+					    </div>
+					    <div class="tab2">
+					        <p>
+						        <img class="imageFlottante" src="img/car2.png" height="210px" alt="autocar"/>
+					        </p>
+						
+					    	<p>
+						        L.C.J - les cars Jouquin  sont implant&eacute;s depuis 80 ans sur la ville
+							    nouvelle de Saint-Quentin-enYvelines, au coeur du Grand Parc de
+							    Versailles &agrave; vingt kilom&egrave;metres de Paris, et exploitent un cenre moderne
+							    d'autocars: <br/>
+							
+							    <br/>Servcices commercial et exploitation, personnel de conduite, atelier,
+							    entretien. <br/>
+                            
+                                <br/>Son expertise et sa rigueur s&eacute;curitaire en mati&egrave;re de multi transports &agrave;
+                                travers l'Europe ont naturellement dessin&eacute; sont titre : Transporteur
+                                Europ&eacute;en.
+                                &agrave; votre service, pour que votre prestation soit unique et son &eacute;x&eacute;cution
+                                r&eacute;gl&eacute;e
+                                &agrave; votre service, pour que votre prestation soit unique et son &eacute;x&eacute;tion
+                                r&eacute;gl&eacute;e dans les moindres d&eacute;tails.<br/>
+                            
+                                <br/><strong>Une &eacute;quipe de professionels du Transport</strong><br/>
+      
+                                <br/>Encad&eacute;e par un Directeur et un Chef d'exploitation d'exp&eacute;rience, notre &eacute;quipe d'Exploitation coordonne 40 chaufeurs de remise
+                                &agrave; vos soins et &agrave; votre service, pour que votre prestation soit unique et son ex&eacute;cution r&eacute;gl&eacute;e dans les moindres d&eacute;tails.	<br/>						
+						
+			    			    <br/><strong>Une large gamme de 50 v&eacute;hicules adapt&eacute;s &agrave; vos besoins de Transport</strong><br/>
+						
+				    		    <br/>L.C.J met &agrave; votre disposition une flotte d'autocars de 8 &agrave; 63 places d'identit&eacute; visuelle uniforme et de moins de 6 ans. Notre
+					    		passion de l'autocar nous a conduit &agrave; orienter notre politique d'entreprise : un parc haut de gamme, r&eacute;cent et soign&eacute;.<br/>
+						
+						        <br/>L'ensemble de nos v&eacute;hicules est liv&eacute; par les deux plus grands constucteurs Evobus (Setra et Mercedes-Benz) et Irisbus
+							    (Renault-Iveco), ce qui permet de b&eacute;n&eacute;ficier des derni&egrave;res avanc&eacute;es technologiques en mati&egrave;re de s&eacute;curit&eacute; et de confort. 
+						    </p>
+					    </div>
+					    <div class="tab3">
+					        <p>
+						        <img class="imageFlottante" src="img/car2.png" height="210px" alt="autocar"/>
+					        </p>
+						
+					    	<p>
+						        L.C.J - les cars Jouquin  sont implant&eacute;s depuis 80 ans sur la ville
+							    nouvelle de Saint-Quentin-enYvelines, au coeur du Grand Parc de
+							    Versailles &agrave; vingt kilom&egrave;metres de Paris, et exploitent un cenre moderne
+							    d'autocars: <br/>
+							
+							    <br/>Servcices commercial et exploitation, personnel de conduite, atelier,
+							    entretien. <br/>
+                            
+                                <br/>Son expertise et sa rigueur s&eacute;curitaire en mati&egrave;re de multi transports &agrave;
+                                travers l'Europe ont naturellement dessin&eacute; sont titre : Transporteur
+                                Europ&eacute;en.
+                                &agrave; votre service, pour que votre prestation soit unique et son &eacute;x&eacute;cution
+                                r&eacute;gl&eacute;e
+                                &agrave; votre service, pour que votre prestation soit unique et son &eacute;x&eacute;tion
+                                r&eacute;gl&eacute;e dans les moindres d&eacute;tails.<br/>
+                            
+                                <br/><strong>Une &eacute;quipe de professionels du Transport</strong><br/>
+      
+                                <br/>Encad&eacute;e par un Directeur et un Chef d'exploitation d'exp&eacute;rience, notre &eacute;quipe d'Exploitation coordonne 40 chaufeurs de remise
+                                &agrave; vos soins et &agrave; votre service, pour que votre prestation soit unique et son ex&eacute;cution r&eacute;gl&eacute;e dans les moindres d&eacute;tails.	<br/>						
+						
+			    			    <br/><strong>Une large gamme de 50 v&eacute;hicules adapt&eacute;s &agrave; vos besoins de Transport</strong><br/>
+						
+				    		    <br/>L.C.J met &agrave; votre disposition une flotte d'autocars de 8 &agrave; 63 places d'identit&eacute; visuelle uniforme et de moins de 6 ans. Notre
+					    		passion de l'autocar nous a conduit &agrave; orienter notre politique d'entreprise : un parc haut de gamme, r&eacute;cent et soign&eacute;.<br/>
+						
+						        <br/>L'ensemble de nos v&eacute;hicules est liv&eacute; par les deux plus grands constucteurs Evobus (Setra et Mercedes-Benz) et Irisbus
+							    (Renault-Iveco), ce qui permet de b&eacute;n&eacute;ficier des derni&egrave;res avanc&eacute;es technologiques en mati&egrave;re de s&eacute;curit&eacute; et de confort. 
+						    </p>
+					    </div>
+					    <div class="tab4">
+					        <p>
+						        <img class="imageFlottante" src="img/car2.png" height="210px" alt="autocar"/>
+					        </p>
+						
+					    	<p>
+						        L.C.J - les cars Jouquin  sont implant&eacute;s depuis 80 ans sur la ville
+							    nouvelle de Saint-Quentin-enYvelines, au coeur du Grand Parc de
+							    Versailles &agrave; vingt kilom&egrave;metres de Paris, et exploitent un cenre moderne
+							    d'autocars: <br/>
+							
+							    <br/>Servcices commercial et exploitation, personnel de conduite, atelier,
+							    entretien. <br/>
+                            
+                                <br/>Son expertise et sa rigueur s&eacute;curitaire en mati&egrave;re de multi transports &agrave;
+                                travers l'Europe ont naturellement dessin&eacute; sont titre : Transporteur
+                                Europ&eacute;en.
+                                &agrave; votre service, pour que votre prestation soit unique et son &eacute;x&eacute;cution
+                                r&eacute;gl&eacute;e
+                                &agrave; votre service, pour que votre prestation soit unique et son &eacute;x&eacute;tion
+                                r&eacute;gl&eacute;e dans les moindres d&eacute;tails.<br/>
+                            
+                                <br/><strong>Une &eacute;quipe de professionels du Transport</strong><br/>
+      
+                                <br/>Encad&eacute;e par un Directeur et un Chef d'exploitation d'exp&eacute;rience, notre &eacute;quipe d'Exploitation coordonne 40 chaufeurs de remise
+                                &agrave; vos soins et &agrave; votre service, pour que votre prestation soit unique et son ex&eacute;cution r&eacute;gl&eacute;e dans les moindres d&eacute;tails.	<br/>						
+						
+			    			    <br/><strong>Une large gamme de 50 v&eacute;hicules adapt&eacute;s &agrave; vos besoins de Transport</strong><br/>
+						
+				    		    <br/>L.C.J met &agrave; votre disposition une flotte d'autocars de 8 &agrave; 63 places d'identit&eacute; visuelle uniforme et de moins de 6 ans. Notre
+					    		passion de l'autocar nous a conduit &agrave; orienter notre politique d'entreprise : un parc haut de gamme, r&eacute;cent et soign&eacute;.<br/>
+						
+						        <br/>L'ensemble de nos v&eacute;hicules est liv&eacute; par les deux plus grands constucteurs Evobus (Setra et Mercedes-Benz) et Irisbus
+							    (Renault-Iveco), ce qui permet de b&eacute;n&eacute;ficier des derni&egrave;res avanc&eacute;es technologiques en mati&egrave;re de s&eacute;curit&eacute; et de confort. 
+						    </p>
+					    </div>
+						<div class="tab5">
+							<p>
+						       <form method="POST" action="">
+									<label for="mdr_nom">Nom de votre soci&eacute;t&eacute;</label><input name="mdr_nom" id="mdr_nom" type="text"/>
+									<br />
+									<label for="mdr_resp">Nom du responsable</label><input name="mdr_resp" id="mdr_resp" type="text"/>
+									<br />
+									<label for="mdr_tel">Num&eacute;ro de t&eacute;l&eacute;phone</label><input name="mdr_tel" id="mdr_tel" type="text"/>
+									<br />
+									<label for="mdr_email">Adresse Email</label><input name="mdr_email" id="mdr_email" type="text"/>
+									<br />
+									<label for="mdr_ville">Ville</label><input name="mdr_ville" id="mdr_ville" type="text"/>
+									<br />
+									<label for="mdr_adresse">Adresse</label><input name="mdr_adresse" id="mdr_adresse" type="text"/>
+									<br />
+									<label for="mdr_fax">Ville</label><input name="mdr_fax" id="mdr_fax" type="text"/>
+									
+									<input name="valid" id="valid" value="-2" type="hidden"/>
+									<br />
+									<button type="submit">R&eacute;f&eacute;rencer ma soci&eacute;t&eacute;</button>
+								</form>
+						    </p>
+					    </div>
+					</div>
+				</div>
+				
+			    
+				<div class="devis">
+				    <h3>Devis imm&eacute;diat</h3>
+					<p>
+					    Vous souhaitez obtenir un devis de l'autocariste
+						<strong>Connex Allier</strong>, 
+						merci de bien vouloir remplir le formulaire ci-dessous:
+					</p>
+			        <form method="" action="">
+					    <fieldset>
+						    <input type="text" name="" id="" placeholder="Votre nom"/>
+						    <input type="text" name="" id="" placeholder="Votre t&eacute;l&eacute;phone"/>
+						    <input type="text" name="" id="" placeholder="Votre adresse email"/>
+						</fieldset>
+						
+						<fieldset>
+						    <input type="text" name="" id="" placeholder="Ville de d&eacute;part "/>
+						    <input type="text" name="" id="" placeholder="Ville d'arriv&eacute;e"/>
+						    <input type="text" name="" id="" placeholder="Nombre de passager"/>
+						</fieldset>
+						
+						<fieldset>
+						    <input type="text" name="" id="" placeholder="Date de d&eacute;part "/>
+						    <input type="text" name="" id="" placeholder="Date de retour"/>
+						</fieldset>
+						
+						<fieldset class="textarea">
+						    <textarea rows="5" cols="48">Commentaire</textarea>
+						    <button type="button">
+							    Envoyer
+							</button>
+						</fieldset>					
+
+					</form>
+			    </div>
+			</div>
+		
+		</div>
+		
+		<?php 
+include_once "inc/bas.inc.php";
+?>
